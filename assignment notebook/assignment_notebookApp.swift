f8aaa82ea9754e9d23681da40fbd1af896a6674a
Benny Chopra <bchopra@bsd220.org>
2025-02-19 09:29:06 -0600

    Initial Commit

---
+++ assignment notebook/assignment_notebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_notebookApp.swift
+//  assignment notebook
+//
+//  Created by Benny Chopra on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct assignment_notebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
